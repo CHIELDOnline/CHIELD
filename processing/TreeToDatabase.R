5c13373ef250e7881bf85729f930716187c5c727
CHIELDOnline <sean.roberts@bristol.ac.uk>
2018-05-25 21:54:02 +0100

    Add extra contributors who don't have a github account

--- processing/TreeToDatabase.R
+++ processing/TreeToDatabase.R
@@ -231,7 +231,12 @@ if(file.exists(extraContributorNamesFile)){
 extraContributorFile = "../data/ExtraContributors.csv"
 if(file.exists(extraContributorFile)){
   extraContributors = read.csv(extraContributorFile, stringsAsFactors = F,encoding = "UTF-8",fileEncoding = "UTF-8")
-  contributors = rbind(contributors,extraContributors)
+  if(nrow(extraContributors)>0){
+    # If username is blank, code breaks
+    # TODO: add full website.
+    extraContributors$username = "x"
+    contributors = rbind(contributors,extraContributors)
+  }
 }
 if(sum(is.na(contributors$username))>0){
   contributors[is.na(contributors$username),]$username = ""
