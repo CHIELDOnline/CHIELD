d43624b1b73e3501c98970e3f845975b2b6e900c
CHIELDOnline <sean.roberts@bristol.ac.uk>
2018-08-10 10:33:23 +1000

    Update page counter: split insert and update commands

--- app/Site/php/pageCounter.php
+++ app/Site/php/pageCounter.php
@@ -2,10 +2,16 @@
 
 $page = $_POST['page'];
 $pdo = new PDO('sqlite:../../data/db/counter.sqlite');
-$sql= "INSERT OR IGNORE INTO counts VALUES (:page1, 0); UPDATE counts SET count = count + 1 WHERE page LIKE :page2";
-$statement=$pdo->prepare($sql);
-$statement->bindParam(':page1', $page, PDO::PARAM_STR);
-$statement->bindParam(':page2', $page, PDO::PARAM_STR);
-$statement->execute();
+
+$insert_sql = "INSERT OR IGNORE INTO counts VALUES (:page, 0)";
+$insert_statement=$pdo->prepare($insert_sql);
+$insert_statement->bindParam(':page', $page, PDO::PARAM_STR);
+$insert_statement->execute();
+
+$update_sql = "UPDATE counts SET count = count + 1 WHERE page = :page";
+$update_statement=$pdo->prepare($update_sql);
+$update_statement->bindParam(':page', $page, PDO::PARAM_STR);
+$update_statement->execute();
+
 echo("DONE");
 ?> 
