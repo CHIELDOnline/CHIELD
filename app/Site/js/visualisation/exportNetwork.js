f44c71d00935966431235bef939b7488d3df2d2d
CHIELDOnline <sean.roberts@bristol.ac.uk>
2018-11-13 20:21:55 +0000

    rebuild

--- app/Site/js/visualisation/exportNetwork.js
+++ app/Site/js/visualisation/exportNetwork.js
@@ -38,3 +38,21 @@ function saveDataAsDOT(includePos = false,directed = true){
   	link.click();
  	document.body.removeChild(link);
 }
+
+
+function saveDataAsDagitty(){
+	var dagittyFormat = visGraphToDagitty(network,network_nodes,network_edges,includePos=true);
+
+	$("body").append(
+	        '<div class="alert alert-success alert-dismissable" style="position:fixed;top:20%;left:10%">'+
+	            '<button type="button" class="close" ' + 
+	                    'data-dismiss="alert" aria-hidden="true">' + 
+	                '&times;' + 
+	            '</button>' + 
+	            '<p>Paste the following into the "model code" window at <a href="http://www.dagitty.net/dags.html">http://www.dagitty.net/dags.html</a></p>' + 
+	            '<textarea cols="100" rows=12>' + 
+	            dagittyFormat +
+	            "</textarea>" +
+	         '</div>');
+
+}
