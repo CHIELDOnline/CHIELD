ed75720f87c61449385212379c22563649d22a8d
CHIELDOnline <sean.roberts@bristol.ac.uk>
2020-08-25 17:18:21 +0100

    Add 'get random link' page

---
+++ app/Site/js/random.js
@@ -0,0 +1,53 @@
+var document_details = [];
+var doc_causal_links = [];
+
+var document_network_layout_options = {
+	hierarchical: {
+	    direction: "LR",
+	    sortMethod: "directed",
+	    levelSeparation: 250
+		}
+	};
+var document_network_physics_options = {
+	hierarchicalRepulsion : {
+		nodeDistance: 50
+	}};
+
+
+function updateRecord(response,type){
+	console.log("updateRecord "+type);
+	if(type=="links"){
+		doc_causal_links = JSON.parse(response);
+		network_nodes.remove(network_nodes.getIds());
+		network_edges.remove(network_edges.getIds());
+		redrawGUIfromObject(doc_causal_links); 
+		network.fit();
+		network.moveTo({scale:3})
+		docKey = doc_causal_links[0].pk;
+		
+		quote = '"' + doc_causal_links[0].Notes + '"' + '<a href="document.html?key='+
+						 + docKey + '"> ... see in context.</a>'
+		
+		$("#quote").html(quote);
+	}
+	
+}
+
+function getRandomQuote(){
+	requestRecord("php/getRandomLinkWithQuote.php", "",'links');
+}
+
+
+$(document).ready(function(){
+	//$("#header").load("header.html"); 
+
+	network_options.layout = document_network_layout_options;
+	network_options.physics = document_network_physics_options;
+	network_options.interaction = {navigationButtons: false};
+	network_options.edges.smooth = false;
+
+	initialiseNetwork();
+	getRandomQuote();
+	
+
+});
---
+++ app/Site/php/getRandomLinkWithQuote.php
@@ -0,0 +1,31 @@
+<?php
+
+$pdo = new PDO('sqlite:../../data/db/CHIELD.sqlite');
+
+$sql= <<<EOT
+SELECT l.pk,
+       s.[name] variable1,
+	   Relation as relation,
+       s2.[name] variable2,
+       Cor,
+       Topic,
+       Stage,
+       Type,
+       Confirmed,
+       Notes
+  FROM causal_links l
+    LEFT JOIN variables s ON l.Var1 = s.pk 
+    LEFT JOIN variables s2 ON l.Var2 = s2.pk
+  WHERE Notes IS NOT NULL AND Notes != ""
+  ORDER BY RANDOM() LIMIT 1;
+EOT;
+
+
+
+$statement=$pdo->prepare($sql);
+$statement->execute();
+$results=$statement->fetchAll(PDO::FETCH_ASSOC);
+$json=json_encode($results);
+echo $json;
+
+?> 
